0c6b4d7889c3f36a13cf64f78b388d219de3f349
anamontiaga <amontiaga@gmail.com>
2019-09-02 14:41:10 +0200

    arrow function on exercise 8

--- 3.1. Métodos funcionales de array/Ejercicio 8 - El ganador de la carrera/main.js
+++ 3.1. Métodos funcionales de array/Ejercicio 8 - El ganador de la carrera/main.js
@@ -5,12 +5,11 @@
 
 const runners = [{ name: "Gregory Goyle", time: 56 }, { name: "Nymphadora Tonks", time: 9 }, { name: "Luna Lovegood", time: 45 }, { name: "Cedric Diggory", time: 28 }, { name: "Cho Chang", time: 35 }];
 
-const max = runners.reduce(function(resultado, valor, indice, array) {
-  if (indice < array.length) {
-    if (resultado < valor) resultado = valor;
-  }
+// const max = runners.reduce(function(resultado, valor) {
+//   if (resultado < valor) resultado = valor;
+//   return resultado;
+// });
 
-  return resultado;
-});
+const max = runners.reduce((resultado, valor) => (resultado < valor ? (resultado = valor) : resultado));
 
-console.log(max);
+console.log(`And the winner is ${max.name}`);
