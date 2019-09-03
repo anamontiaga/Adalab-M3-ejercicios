a63c0fa028eeb3dcc1a51946b19ec52c546031c7
anamontiaga <amontiaga@gmail.com>
2019-09-03 10:10:52 +0200

    add BONUS

--- 3.1. Métodos funcionales de array/BONUS - sort ()/main.js
+++ 3.1. Métodos funcionales de array/BONUS - sort ()/main.js
@@ -3,16 +3,15 @@
 
 const users = [{ name: "María", isPremium: false, pin: 2389 }, { name: "Lucía", isPremium: true, pin: 2384 }, { name: "Susana", isPremium: true, pin: 2837 }, { name: "Rocío", isPremium: false, pin: 5232 }, { name: "Inmaculada", isPremium: false, pin: 8998 }];
 
-function compare(a, b) {
+const sortByName = (a, b) => {
   if (a.name < b.name) {
     return -1;
-  }
-  if (a.name > b.name) {
+  } else if (a.name > b.name) {
     return 1;
+  } else {
+    return 0;
   }
-  return 0;
-}
+};
+const sortedLanguages = users.sort(sortByName);
 
-users.sort(compare);
-
-console.log(users);
+console.log(sortedLanguages);
