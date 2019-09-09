6613866624e04295edc858005393395a1a395aac
anamontiaga <amontiaga@gmail.com>
2019-09-09 18:53:23 +0200

    not finished exercised 3

--- 3.6. Eventos en React/Ejercicio 3 - Expreso mi odio en rojo/ejercicio-3/src/OnionHater.js
+++ 3.6. Eventos en React/Ejercicio 3 - Expreso mi odio en rojo/ejercicio-3/src/OnionHater.js
@@ -2,26 +2,39 @@ import React from "react";
 import "./OnionHater.css";
 
 class OnionHater extends React.Component {
-  //   constructor(isHating) {
-  //     super(isHating);
-  //     this.isHating = this.isHating.bind(this);
-  //   }
+  constructor(props) {
+    super(props);
+    this.handleWrite = this.handleWrite.bind(this);
+  }
 
   // this.props.isHating = "true" &&
 
   handleWrite(event) {
     if (event.target.value === "cebolla") {
+      this.isHating = true;
+    } else {
+      this.isHating = false;
+    }
+    this.forceUpdate();
+    /*console.log(this.isHating, event.target.value);
+    if (this.isHating === true) {
       document.body.style.backgroundColor = "red";
     } else {
       document.body.style.backgroundColor = "white";
-    }
+    }*/
   }
   render() {
+    if (this.isHating === true) {
     return (
-      <div className="screen">
+      <div className="Screen red">
         <textarea className="selectCities" name="textarea" rows="10" cols="50" placeholder="Write something here..." onKeyPress={this.handleWrite}></textarea>
       </div>
-    );
+    ) else {
+      return (
+      <div className="Screen ">
+        <textarea className="selectCities" name="textarea" rows="10" cols="50" placeholder="Write something here..." onKeyPress={this.handleWrite}></textarea>
+      </div>
+      )}
   }
 }
 
--- 3.6. Eventos en React/lifting-murray-example/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import MurrayList from "./MurrayList";
-
-ReactDOM.render(<MurrayList />, document.getElementById("react-root"));
-// serviceWorker.unregister();
