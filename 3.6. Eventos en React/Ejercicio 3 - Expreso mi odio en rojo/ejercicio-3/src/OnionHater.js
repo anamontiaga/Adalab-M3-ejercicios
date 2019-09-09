77f25d449a8a0d5dc7ade398dae6fabdfeed7d0c
anamontiaga <amontiaga@gmail.com>
2019-09-09 19:03:50 +0200

    add changes to exercise 3

--- 3.6. Eventos en React/Ejercicio 3 - Expreso mi odio en rojo/ejercicio-3/src/OnionHater.js
+++ 3.6. Eventos en React/Ejercicio 3 - Expreso mi odio en rojo/ejercicio-3/src/OnionHater.js
@@ -7,8 +7,6 @@ class OnionHater extends React.Component {
     this.handleWrite = this.handleWrite.bind(this);
   }
 
-  // this.props.isHating = "true" &&
-
   handleWrite(event) {
     if (event.target.value === "cebolla") {
       this.isHating = true;
@@ -16,25 +14,21 @@ class OnionHater extends React.Component {
       this.isHating = false;
     }
     this.forceUpdate();
-    /*console.log(this.isHating, event.target.value);
-    if (this.isHating === true) {
-      document.body.style.backgroundColor = "red";
-    } else {
-      document.body.style.backgroundColor = "white";
-    }*/
   }
   render() {
     if (this.isHating === true) {
-    return (
-      <div className="Screen red">
-        <textarea className="selectCities" name="textarea" rows="10" cols="50" placeholder="Write something here..." onKeyPress={this.handleWrite}></textarea>
-      </div>
-    ) else {
       return (
-      <div className="Screen ">
-        <textarea className="selectCities" name="textarea" rows="10" cols="50" placeholder="Write something here..." onKeyPress={this.handleWrite}></textarea>
-      </div>
-      )}
+        <div className="Screen red">
+          <textarea className="selectCities" name="textarea" rows="10" cols="50" placeholder="Write something here..." onKeyPress={this.handleWrite}></textarea>
+        </div>
+      );
+    } else {
+      return (
+        <div className="Screen ">
+          <textarea className="selectCities" name="textarea" rows="10" cols="50" placeholder="Write something here..." onKeyPress={this.handleWrite}></textarea>
+        </div>
+      );
+    }
   }
 }
 
