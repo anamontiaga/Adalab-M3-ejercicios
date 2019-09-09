1757d72791f429d16dd5f7c09fefc5f842c469cf
anamontiaga <amontiaga@gmail.com>
2019-09-09 23:54:23 +0200

    add lifting example

--- 3.6. Eventos en React/Ejercicio 5 - Ciudades/ejercicio-5/src/index.js
+++ 3.6. Eventos en React/Ejercicio 5 - Ciudades/ejercicio-5/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CitySelector from "./CitySelector";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CitySelector />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Para terminar de entender bien cómo funciona el lifting vamos a hacer un ejercicio muy sencillo. Partimos de un select con nombre de ciudades que encapsulamos en un componente CitySelector. Vamos a hacer que, al seleccionar una ciudad del select, aparezca una foto de la misma al lado. La diferencia con ejercicios anteriores es que ahora el select está en su propio componente. Para llevarlo a cabo debemos:
+// guardar en un atributo de la clase la ciudad seleccionada inicial, por ejemplo, this.selectedCity = 'Madrid' y usarlo para pintar la imagen en el render
+// crear un método handleClick que actualice el valor de selectedCity y llame a forceUpdate para forzar el repintado de la imagen
+// y usar lifting para pasarlo al componente hijo que se ejecute al cambiar el select
---
+++ 3.6. Eventos en React/lifting-random-murray/src/RandomMurray.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const getRandomInteger = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+const MIN_SIZE = 200;
+const MAX_SIZE = 400;
+
+class RandomMurray extends React.Component {
+  render() {
+    const randomMurray = getRandomInteger(MIN_SIZE, MAX_SIZE);
+
+    return <img src={`http://www.fillmurray.com/200/${randomMurray}`} alt="Random murray" />;
+  }
+}
+
+export default RandomMurray;
