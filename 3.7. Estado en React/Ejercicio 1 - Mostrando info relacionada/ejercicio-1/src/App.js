a5f198460f8bf46ab1260378f0ea26e782b7bfeb
anamontiaga <amontiaga@gmail.com>
2019-09-10 15:05:53 +0200

    finish exercice 1

--- 3.7. Estado en React/Ejercicio 1 - Mostrando info relacionada/ejercicio-1/src/App.js
+++ 3.7. Estado en React/Ejercicio 1 - Mostrando info relacionada/ejercicio-1/src/App.js
@@ -1,46 +1,70 @@
 import React from "react";
 import "./App.css";
 
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: ""
+//     };
+
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   // //version corta
+//   // handleChange(event) {
+//   //   let valueInput = event.target.value;
+
+//   //   this.setState((prevState, props) => ({
+//   //     value: prevState.value === "" ? `${valueInput}` : `${valueInput}`
+//   //   }));
+//   //   console.log(this.state.value);
+//   // }
+//   handleChange(event) {
+//     let valueInput = event.target.value;
+
+//     this.setState((prevState, props) => {
+//       let nextValue;
+//       if (prevState.value === "") {
+//         nextValue = valueInput;
+//       } else {
+//         nextValue = `${valueInput}`; // traducir, qué diferencia hay con lo anterior?
+//       }
+//       return {
+//         value: nextValue
+//       };
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <input name="input" onChange={this.handleChange}></input>
+//         <p className="text">{this.state.value}</p>
+//       </>
+//     );
+//   }
+// }
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       value: ""
     };
-
     this.handleChange = this.handleChange.bind(this);
   }
-  // //version corta
-  // handleChange(event) {
-  //   let valueInput = event.target.value;
 
-  //   this.setState((prevState, props) => ({
-  //     value: prevState.value === "" ? `${valueInput}` : `${valueInput}`
-  //   }));
-  //   console.log(this.state.value);
-  // }
-  handleChange(event) {
-    let valueInput = event.target.value;
-
-    this.setState((prevState, props) => {
-      let nextValue;
-      if (prevState.value === "") {
-        nextValue = valueInput;
-      } else {
-        nextValue = `${valueInput}`; // traducir, qué diferencia hay con lo anterior?
-      }
-      return {
-        value: nextValue
-      };
+  handleChange(ev) {
+    this.setState({
+      value: ev.target.value
     });
   }
-
   render() {
     return (
-      <>
-        <input name="input" onChange={this.handleChange}></input>
-        <p className="text">{this.state.value}</p>
-      </>
+      <div className="App">
+        <input type="text" onChange={this.handleChange} />
+        <p>{this.state.value}</p>
+      </div>
     );
   }
 }
