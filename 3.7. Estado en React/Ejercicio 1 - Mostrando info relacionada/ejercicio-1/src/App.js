fa6c0beb92104a0018a2d4bce522de0033af95a0
anamontiaga <amontiaga@gmail.com>
2019-09-10 12:21:30 +0200

    finish exercise 1

--- 3.7. Estado en React/Ejercicio 1 - Mostrando info relacionada/ejercicio-1/src/App.js
+++ 3.7. Estado en React/Ejercicio 1 - Mostrando info relacionada/ejercicio-1/src/App.js
@@ -4,21 +4,43 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
-    this.handleClick = this.handleClick.bind(props);
+    this.state = {
+      value: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  // //version corta
+  // handleChange(event) {
+  //   let valueInput = event.target.value;
+
+  //   this.setState((prevState, props) => ({
+  //     value: prevState.value === "" ? `${valueInput}` : `${valueInput}`
+  //   }));
+  //   console.log(this.state.value);
+  // }
+  handleChange(event) {
+    let valueInput = event.target.value;
+
+    this.setState((prevState, props) => {
+      let nextValue;
+      if (prevState.value === "") {
+        nextValue = valueInput;
+      } else {
+        nextValue = `${valueInput}`; // traducir, qué diferencia hay con lo anterior?
+      }
+      return {
+        value: nextValue
+      };
+    });
   }
 
-  handleClick(e) {
-    console.log("Hola");
-    this.setState({ inputText: e.target.value });
-  }
   render() {
-    let inputText = this.state.inputText;
     return (
-      <div className="App">
-        <input onChange={this.handleClick} type="text"></input>
-        <p>{inputText}</p>
-      </div>
+      <>
+        <input name="input" onChange={this.handleChange}></input>
+        <p className="text">{this.state.value}</p>
+      </>
     );
   }
 }
