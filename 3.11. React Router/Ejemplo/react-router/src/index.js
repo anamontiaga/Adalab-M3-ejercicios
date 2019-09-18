d3f7c6f974920af53c8bd360258d70e1a5b75120
anamontiaga <amontiaga@gmail.com>
2019-09-18 13:04:25 +0200

    router install

--- 3.11. React Router/Ejemplo/react-router/src/index.js
+++ 3.11. React Router/Ejemplo/react-router/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// 1 - INSTALAMOS HASHROUTER
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// 2- ENVOLVEMOS NUESTRA APP CON EL ROUTER
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
