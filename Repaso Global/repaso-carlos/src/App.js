2cd403419959e71738993a6b65938276f0196ec5
anamontiaga <amontiaga@gmail.com>
2019-09-21 20:24:52 +0200

    project prepared

---
+++ Repaso Global/repaso-carlos/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="app">:)</div>;
+  }
+}
+
+export default App;
