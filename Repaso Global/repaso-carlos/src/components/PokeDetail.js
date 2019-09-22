3d5c6c7c5649841d2c2a4ef76b7a4bf793384325
anamontiaga <amontiaga@gmail.com>
2019-09-22 21:39:08 +0200

    improvements on router

--- Repaso Global/repaso-carlos/src/components/PokeDetail.js
+++ Repaso Global/repaso-carlos/src/components/PokeDetail.js
@@ -6,31 +6,54 @@ const PokeDetail = props => {
   const { routerProps, pokemones } = props;
   const pokeId = parseInt(routerProps.match.params.pokeId);
 
-  const pokemon = pokemones.filter(item => item.id === pokeId);
-  const { name, url, types } = pokemon[0];
+  if (pokeId > pokemones.length) {
+    return (
+      <React.Fragment>
+        <p>Mira, mira, que es viernes y me estás...</p>
+        <Link to="/" className="app__back">
+          Volver al listado, listo :)
+        </Link>
+      </React.Fragment>
+    );
+  }
 
-  return (
-    <React.Fragment>
-      <div className="poke-detail">
-        <h2 className="poke-detail__name">{name}</h2>
-        <div className="poke-detail__img">
-          <img src={url} alt={name}></img>
+  const pokemon = pokemones.filter(item => item.id === pokeId);
+
+  if (pokemon[0]) {
+    const { name, url, types } = pokemon[0];
+
+    return (
+      <React.Fragment>
+        <div className="poke-detail">
+          <h2 className="poke-detail__name">{name}</h2>
+          <div className="poke-detail__img">
+            <img src={url} alt={name}></img>
+          </div>
+          <ul className="poke-detail__types">
+            {types.map((type, index) => {
+              return (
+                <li className="poke-detail__type" key={index}>
+                  {type}
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        <ul className="poke-detail__types">
-          {types.map((type, index) => {
-            return (
-              <li className="poke-detail__type" key={index}>
-                {type}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <Link to="/" className="app__back">
-        Volver al listado
-      </Link>
-    </React.Fragment>
-  );
+        <Link to="/" className="app__back">
+          Volver al listado
+        </Link>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <p>Chacho, ese no lo tienes</p>
+        <Link to="/" className="app__back">
+          Volver al listado
+        </Link>
+      </React.Fragment>
+    );
+  }
 };
 
 PokeDetail.propTypes = {
